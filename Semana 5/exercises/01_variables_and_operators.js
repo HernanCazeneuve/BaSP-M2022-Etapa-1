eb9128449638b4115d51f476551467e27aa02c0b
hernan cazeneuve <hernancazeneuve@gmail.com>
2022-04-16 12:40:36 -0300

    BaSP-2022: ex01-variables and operators

---
+++ Semana 5/exercises/01_variables_and_operators.js
@@ -0,0 +1,10 @@
+var var1 = 3;
+var var2 = 4;
+var var3 = var1 + var2;
+console.log("ex01-variables and operators-A:", var3)
+var string1 = "hola ";
+var string2 = "mundo";
+var string3 = string1 + string2;
+console.log("ex01-variables and operators-B:", string3)
+var lenghtStrings = string3.length;
+console.log("ex01-variables and operators-C:", lenghtStrings);
